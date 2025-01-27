aff009d3ae2bfaa319e2775cbb844a5daba64e88
Siddhartha reddy <siddharthanayaki34@gmail.com>
2025-01-27 22:10:49 +0530

    Do while loop

---
+++ Demo007.java
@@ -0,0 +1,13 @@
+class Demo007 {
+    @SuppressWarnings("StringEquality")
+    public static void main(String[] args) {
+
+        String Username = "siddu";
+        String Password = "siddu_1";
+
+        while((Username == "siddu") && (Password == "siddu_1")) {
+            System.out.println("Welcome to Mallareddy university");
+            break;
+        }
+    }
+}
