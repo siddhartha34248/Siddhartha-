55f6e81fe2638c643fe486ae098631d987237362
Siddhartha reddy <siddharthanayaki34@gmail.com>
2025-01-27 22:12:09 +0530

    Area of Circle

---
+++ Demo013.java
@@ -0,0 +1,9 @@
+class Demo013 {
+    public static void main(String args[]){
+        int base = 20;
+        int height = 30;
+        double area = (0.5)*base*height;
+        System.out.println(area);
+    }
+
+}
