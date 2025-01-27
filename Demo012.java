2ff836d6e001434888e81793530cbff839d579f7
Siddhartha reddy <siddharthanayaki34@gmail.com>
2025-01-27 22:11:36 +0530

    Area of Triangle

---
+++ Demo012.java
@@ -0,0 +1,7 @@
+ class Demo012 {
+    public static void main(String[] args){
+        int radius = 7;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("Area of the Circle "+ radius + "is"+ area);
+ }
+}
