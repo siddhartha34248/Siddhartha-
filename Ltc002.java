b271c13d01272547d6c82551edd5ddd40925ffc8
Siddhartha reddy <siddharthanayaki34@gmail.com>
2025-01-27 22:44:21 +0530

    Reverse integer

---
+++ Ltc002.java
@@ -0,0 +1,27 @@
+public class Ltc002 {
+    public int reverse(int x) {
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+            reversed = reversed * 10 + digit;
+        }
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Ltc002 solution = new Ltc002();
+        System.out.println(solution.reverse(123));
+        System.out.println(solution.reverse(-123));
+        System.out.println(solution.reverse(120));
+        System.out.println(solution.reverse(1534236469));
+    }
+}
+
