0cb8b72882e9072943f7c1e8b6dc7baaf5d7534e
Siddhartha reddy <siddharthanayaki34@gmail.com>
2025-01-27 22:46:43 +0530

    Plus one

---
+++ Ltc007.java
@@ -0,0 +1,27 @@
+class Ltc007{
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[digits.length + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+
+    public static void main(String[] args) {
+        Ltc007 ltcode007 = new Ltc007();
+
+        int[] digits = {9, 9, 9};
+        int[] result = ltcode007.plusOne(digits);
+
+        System.out.print("Result: ");
+        for (int num : result) {
+            System.out.print(num);
+        }
+    }
+}
